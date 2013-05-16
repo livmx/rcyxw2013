116235c17c35a0bbba3fd3825338b2b842a40305
yiqing-95 <yiqing95@yahoo.com.cn>
2013-05-16 17:30:30 +0800

    切换到另一个branch去

---
+++ protected/modules/blog/install/BlogInstaller.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: yiqing
+ * Date: 12-8-24
+ * Time: 下午12:19
+ * To change this template use File | Settings | File Templates.
+ */
+class BlogInstaller extends BaseModuleInstaller
+{
+
+    public function install(){
+
+        echo __METHOD__;
+
+    }
+
+    public function uninstall(){
+        echo __METHOD__;
+    }
+
+
+}
