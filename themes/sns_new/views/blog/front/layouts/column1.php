84ad598cb9384bfe1241f660d1e9463320a37d73
yiqing-95 <yiqing95@yahoo.com.cn>
2013-05-24 01:30:59 +0800

    修改标签云

--- themes/sns_new/views/blog/front/layouts/column1.php
+++ themes/sns_new/views/blog/front/layouts/column1.php
@@ -1,7 +1,30 @@
 <?php $this->beginContent('/layouts/main'); ?>
 <div class="container">
-	<div id="content">
-		<?php echo $content; ?>
-	</div><!-- content -->
+
+    <div class="container site-body">
+
+        <div class="cell">
+
+            <?php if(Layout::hasRegion('rightSideBar')): ?>
+
+                <div class="col size7of9">
+                    <?php echo $content; ?>
+                </div>
+
+                <div class="col sizefill">
+                    <?php Layout::renderRegion('rightSideBar'); ?>
+                </div>
+
+            <?php else:?>
+
+                <div class="col">
+                    <?php echo $content; ?>
+                </div>
+
+            <?php  endif; ?>
+
+        </div>
+
+    </div>
 </div>
 <?php $this->endContent(); ?>
--- themes/sns_new/views/blog/front/post/index.php
+++ themes/sns_new/views/blog/front/post/index.php
@@ -1,18 +1,75 @@
 <?php
-$this->breadcrumbs=array(
-	'Posts',
+$this->breadcrumbs = array(
+    'Posts',
 );
 
-$this->menu=array(
-	array('label'=>'Create Post', 'url'=>array('create')),
-	array('label'=>'Manage Post', 'url'=>array('admin')),
+$this->menu = array(
+    array('label' => 'Create Post', 'url' => array('create')),
+    array('label' => 'Manage Post', 'url' => array('admin')),
 );
 ?>
 
-<h1>Posts</h1>
+    <h1>Posts</h1>
 
 <?php $this->widget('zii.widgets.CListView', array(
-	'dataProvider'=>$dataProvider,
-	'itemView'=>'_view',
-	'ajaxUpdate'=>false,
+    'dataProvider' => $dataProvider,
+    'itemView' => '_view',
+    'ajaxUpdate' => false,
 )); ?>
+
+
+<?php Layout::beginBlock('rightSideBar'); ?>
+
+<?php YsPageBox::beginPanelWithHeader(array('header' => ' 标签云')) ?>
+    <div class="cell">
+        <?php $this->widget('UserMenu'); ?>
+
+        <?php
+        /*
+        $this->widget('TagCloud', array(
+            'maxTags' => 15,
+        ));
+        */
+        ?>
+
+        <?php
+
+        $arrTags = array();
+        $tags=Tag::model()->findTagWeights(18);
+
+        foreach($tags as $tag=>$weight)
+        {
+            $arrTags[$tag] = array(
+                'weight'=>$weight,
+                'url'=>$this->createUrl('post/index',array('tag'=>str_replace(' ','-',  trim($tag)))),
+                 'htmlOptions'=>array(
+                     'target'=>'_self',
+                 )
+            );
+        }
+
+        $this->widget('application.extensions.YiiTagCloud.YiiTagCloud',
+            array(
+                'beginColor' => '00089A',
+                'endColor' => 'A3AEFF',
+                'minFontSize' => 8,
+                'maxFontSize' => 20,
+                'arrTags' => $arrTags,
+            )
+        );
+        ?>
+
+    </div>
+
+<?php YsPageBox::endPanel(); ?>
+
+<?php YsPageBox::beginPanelWithHeader(array('header' => '最近评论')) ?>
+
+<?php $this->widget('RecentComments', array(
+    'maxComments' => Yii::app()->params['recentCommentCount'],
+)); ?>
+
+<?php YsPageBox::endPanel(); ?>
+
+
+<?php Layout::endBlock(); ?>
